d4cbcca579ba113113427ad209a6605957f5f278
Rafael Menezes <menezes.carloseng@gmail.com>
2021-05-02 14:28:25 -0300

    placar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { OpcaoComponent } from './jogo-da-velha/opcao/opcao.component';
 import { ContainerComponent } from './jogo-da-velha/container/container.component';
 import { PlayerComponent } from './jogo-da-velha/player/player.component';
+import { PlacarComponent } from './jogo-da-velha/placar/placar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OpcaoComponent,
     ContainerComponent,
-    PlayerComponent
+    PlayerComponent,
+    PlacarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jogo-da-velha/placar/placar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlacarComponent } from './placar.component';
+
+describe('PlacarComponent', () => {
+  let component: PlacarComponent;
+  let fixture: ComponentFixture<PlacarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlacarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlacarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
