f641436eb370845d811b72dac24e7515a6a81cdf
Rafael Menezes <menezes.carloseng@gmail.com>
2021-05-02 14:25:03 -0300

    componente player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OpcaoComponent } from './jogo-da-velha/opcao/opcao.component';
 import { ContainerComponent } from './jogo-da-velha/container/container.component';
+import { PlayerComponent } from './jogo-da-velha/player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OpcaoComponent,
-    ContainerComponent
+    ContainerComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
