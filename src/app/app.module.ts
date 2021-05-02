6e86d4e4cb11ff994516ffafcf43da63ff5db4a3
Rafael Menezes <menezes.carloseng@gmail.com>
2021-05-02 13:50:09 -0300

    opçao componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpcaoComponent } from './jogo-da-velha/opcao/opcao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpcaoComponent
   ],
   imports: [
     BrowserModule,
